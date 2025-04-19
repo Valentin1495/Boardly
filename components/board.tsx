75a4b38fb5ca2b3f43e3ca38a5d632824ba020a8
Valentin1495 <newface95@icloud.com>
2025-04-19 23:35:37 +0900

    Reset

--- components/board.tsx
+++
@@ -1,8 +0,0 @@
-import { fetchTodosByStatus } from '@/actions/todo.action';
-
-export default async function Board() {
-  const todos = await fetchTodosByStatus();
-  console.log(todos);
-
-  return <div>board</div>;
-}
